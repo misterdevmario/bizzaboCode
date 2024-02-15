c2a6c05ecca29c43dd89061b03f2839e20ad3e42
Mario <mariorodriguez1110@gmail.com>
2024-02-15 10:46:41 -0600

    hackerrank-getTotalX

--- hackerrank/getTotalX.js
+++ hackerrank/getTotalX.js
@@ -1,27 +1,22 @@
-const a = [2, 4];
-const b = [16, 32, 96];
+const a = [2, 6];
+const b = [24, 36];
 
 function getTotalX(a, b) {
+  const factorA = [];
+  const factorB = [];
+  const factors = [];
+
   // Write your code here
-  let factorA = [];
-  let factorB = [];
-  let factors = 0;
-  for (let i = 1; i < 100; i++) {
-    for (let j = 0; j < a.length; j++) {
-      factorA.push(a[j] * i);
-    }
-    for (let k = 0; k < b.length; k++) {
-      if (b[k] % i == 0) factorB.push(b[k] / i);
-      
+  for (let i = 0; i < 10; i++) {
+    for (let j = 1; j < 100; j++) {
+      if (a[i] !== undefined && a[i] % j == 0) factorA.push(j);
+      if (b[i] !== undefined && b[i] % j == 0) factorB.push(j);
     }
   }
+  const arrAB = [...a, ...b];
+  const commonFactors = Array.from(new Set([...factorA, ...factorB]));
 
-  let eliminateDuplicatedFactorA = new Set(factorA);
-  let uniqueValuesFactorA = Array.from(eliminateDuplicatedFactorA);
-  let eliminateDuplicatedFactorB = new Set(factorB);
-  let uniqueValuesFactorB = Array.from(eliminateDuplicatedFactorB);
-console.log(uniqueValuesFactorA)
-console.log(uniqueValuesFactorB)
+console.log(arrAB, commonFactors)
 }
 
 getTotalX(a, b);
