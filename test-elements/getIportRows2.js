9615331e55478b85d74dfc2e4ad3062c73e9164b
Mario <mariorodriguez1110@gmail.com>
2024-09-05 15:10:48 -0600

    Main side

---
+++ test-elements/getIportRows2.js
@@ -0,0 +1,276 @@
+{
+	"result": [
+		{
+			"email": "agata.wawrzynczak@amu.edu.pl",
+			"matchId": "105784797"
+		},
+		{
+			"email": "akocaysel@yahoo.com",
+			"matchId": "107343537"
+		},
+		{
+			"email": "akram.keramat@bio.uni-stuttgart.de",
+			"matchId": "106801052"
+		},
+		{
+			"email": "andres@luxembourg.co.il",
+			"matchId": "103889055"
+		},
+		{
+			"email": "andrew_zumbusch@cargill.com",
+			"matchId": "83919439"
+		},
+		{
+			"email": "aradc059@uottawa.ca",
+			"matchId": "107155177"
+		},
+		{
+			"email": "asma.kasmani@crl.com",
+			"matchId": "107118703"
+		},
+		{
+			"email": "blee28@its.jnj.com",
+			"matchId": "88411397"
+		},
+		{
+			"email": "caroline.parneix@loreal.com",
+			"matchId": "102215660"
+		},
+		{
+			"email": "chertopalov@fzu.cz",
+			"matchId": "98507545"
+		},
+		{
+			"email": "choriegomarin.1@buckeyemail.osu.edu",
+			"matchId": "107351349"
+		},
+		{
+			"email": "claire.patterson@sedapds.com",
+			"matchId": "82100155"
+		},
+		{
+			"email": "danaculita@yahoo.co.uk",
+			"matchId": "107123923"
+		},
+		{
+			"email": "debprasad.neuro@gmail.com",
+			"matchId": "107349399"
+		},
+		{
+			"email": "div.chauhan87@gmail.com",
+			"matchId": "94776522"
+		},
+		{
+			"email": "dmetzler@its.jnj.com",
+			"matchId": "96523465"
+		},
+		{
+			"email": "ejaj_ahmad@rediffmail.com",
+			"matchId": "107218005"
+		},
+		{
+			"email": "fabienne.praxmarer@losan.de",
+			"matchId": "94337229"
+		},
+		{
+			"email": "fouzia.machrouhi-porcher@merckgroup.com",
+			"matchId": "106825958"
+		},
+		{
+			"email": "g_yuksel@ogr.eskisehir.edu.tr",
+			"matchId": "107346912"
+		},
+		{
+			"email": "georgina.x.armstrong@gsk.com",
+			"matchId": "94769212"
+		},
+		{
+			"email": "guveng15@itu.edu.tr",
+			"matchId": "107348382"
+		},
+		{
+			"email": "haraya@nihs.go.jp",
+			"matchId": "107149224"
+		},
+		{
+			"email": "heather.mcauliffe@cormica.com",
+			"matchId": "104913951"
+		},
+		{
+			"email": "heeralal.bassi@biontech.de",
+			"matchId": "97479160"
+		},
+		{
+			"email": "huey.lee@siemens-healthineers.com",
+			"matchId": "83603444"
+		},
+		{
+			"email": "ingela.carlen@malvernpanalytical.com",
+			"matchId": "98646460"
+		},
+		{
+			"email": "katy.dickinson@cormica.com",
+			"matchId": "105784416"
+		},
+		{
+			"email": "lamtuongvi.tran@uqconnect.edu.au",
+			"matchId": "106975483"
+		},
+		{
+			"email": "lee_byung_kook@lilly.com",
+			"matchId": "34968132"
+		},
+		{
+			"email": "leonardu002@e.ntu.edu.sg",
+			"matchId": "107236440"
+		},
+		{
+			"email": "mayuribirajdar388@gmail.com",
+			"matchId": "103797413"
+		},
+		{
+			"email": "meltemipekci@gmail.com",
+			"matchId": "undefined"
+		},
+		{
+			"email": "meltemterzioglu@kale.com.tr",
+			"matchId": "92359592"
+		},
+		{
+			"email": "mfrancacamila@usp.br",
+			"matchId": "96349424"
+		},
+		{
+			"email": "michael.fallon@sgs.com",
+			"matchId": "92699914"
+		},
+		{
+			"email": "monika.prasnikar@ffa.uni-lj.si",
+			"matchId": "102309473"
+		},
+		{
+			"email": "morgan.delong@sri.com",
+			"matchId": "94941008"
+		},
+		{
+			"email": "muhammadayyaz333@yahoo.com",
+			"matchId": "107290137"
+		},
+		{
+			"email": "neil.forbes@vectura.com",
+			"matchId": "94471771"
+		},
+		{
+			"email": "nelli.laleni@malvernpanalytical.com",
+			"matchId": "98369362"
+		},
+		{
+			"email": "nick.watterson@malvernpanalytical.com",
+			"matchId": "104047827"
+		},
+		{
+			"email": "nicole.mackala@curevac.com",
+			"matchId": "98148732"
+		},
+		{
+			"email": "nikola.ilic@vin.bg.ac.rs",
+			"matchId": "107224446"
+		},
+		{
+			"email": "nshukla8@its.jnj.com",
+			"matchId": "106033555"
+		},
+		{
+			"email": "oguz.yucel@iuc.edu.tr",
+			"matchId": "104326372"
+		},
+		{
+			"email": "olivia.ke@omec-instruments.com",
+			"matchId": "101603059"
+		},
+		{
+			"email": "onurzirhli@sabanciuniv.edu",
+			"matchId": "107344878"
+		},
+		{
+			"email": "priya.gupta@malvernpanalytical.com",
+			"matchId": "104274703"
+		},
+		{
+			"email": "ptrw@novonordisk.com",
+			"matchId": "107130396"
+		},
+		{
+			"email": "qaisersaleem071@gmail.com",
+			"matchId": "103709756"
+		},
+		{
+			"email": "remijn.sandra@sysmex.nl",
+			"matchId": "27346486"
+		},
+		{
+			"email": "sattar.eghbalian@gmail.com",
+			"matchId": "99790875"
+		},
+		{
+			"email": "selin.daglar@sabanciuniv.edu",
+			"matchId": "107341965"
+		},
+		{
+			"email": "sevgican-91@hotmail.com",
+			"matchId": "107353912"
+		},
+		{
+			"email": "shanthi_ml@yahoo.co.in",
+			"matchId": "26155614"
+		},
+		{
+			"email": "shaoqiu.zhuo@revance.com",
+			"matchId": "104557492"
+		},
+		{
+			"email": "shielann.baruti@aeciworld.com",
+			"matchId": "97997959"
+		},
+		{
+			"email": "spomenka.milak@curevac.com",
+			"matchId": "102334632"
+		},
+		{
+			"email": "sru@buhlmannlabs.ch",
+			"matchId": "107117095"
+		},
+		{
+			"email": "suleymanova.samira1990@gmail.com",
+			"matchId": "88012345"
+		},
+		{
+			"email": "tamiris.figueiredo@debiopharm.com",
+			"matchId": "102906317"
+		},
+		{
+			"email": "thoa@fertin.com",
+			"matchId": "98833810"
+		},
+		{
+			"email": "vmserrano@inescop.es",
+			"matchId": "104840280"
+		},
+		{
+			"email": "yldyorulmaz@sabanciuniv.edu",
+			"matchId": "92288996"
+		},
+		{
+			"email": "yuqing.zhu@henkel.com",
+			"matchId": "102424878"
+		},
+		{
+			"email": "zehra.cobandede@sabanciuniv.edu",
+			"matchId": "107341578"
+		},
+		{
+			"email": "zsuzsanna.foldvari@gmail.com",
+			"matchId": "107354272"
+		}
+	]
+}
